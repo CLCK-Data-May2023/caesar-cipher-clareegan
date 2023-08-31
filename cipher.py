82616368c7b3056b08d9c0cacf97f91a544f6d8d
clur97 <143642118+clur97@users.noreply.github.com>
2023-08-31 12:47:10 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,29 @@
-# add your code here
+def encrypt_text(plaintext,n):
+    ans = ""
+    # iterate over the given text
+    for i in range(len(plaintext)):
+        ch = plaintext[i]
+        
+        # check if space is there then simply add space
+        if ch==" ":
+            ans+=" "
+        # check if a character is uppercase then encrypt it accordingly 
+        elif (ch.isupper()):
+            ans += chr((ord(ch) + n-65) % 26 + 65)
+        # check if a character is lowercase then encrypt it accordingly
+        
+        else:
+            ans += chr((ord(ch) + n-97) % 26 + 97)
+    
+    return ans
+
+plaintext = "HELLO EVERYONE"
+n = 5
+print("Plain Text is : " + plaintext)
+print("Shift pattern is : " + str(n))
+print("Cipher Text is : " + encrypt_text(plaintext,n))
+
+txt = input("Please enter a sentence: ")  
+s = 5
+
+print("The encrypted sentence is: " + encrypt_text(plaintext,n))
